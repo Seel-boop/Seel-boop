3f841700036b788caf4d75803644e472cda34720
Seel-boop <82305847+Seel-boop@users.noreply.github.com>
2021-04-11 23:52:56 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,14 +1,20 @@
-import time
+#replace will replace all vowels and duplicate letters except for the first instance of those letters.
 
+
+
+import time
+# Removes the vowels
 def word_no_voul():
     sentence = input("words: ")
+    # By cycling through a list of the vowels
     remove = ["a", "e", "e", "i", "o", "u"]
     length = len(remove)
     global new_sentence
     new_sentence = sentence
     for i in range(6):
+        # And replacing them
         new_sentence = new_sentence.replace(remove[i], "")
-
+# Removes duplicate letters except for the first instance of the letters.
 def word_no_dupe():
     final = new_sentence
     final1 = len(final)
